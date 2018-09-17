e82df6ca8fb45fe79189dc7062de1bc5ca6aa505
Lucas Albuquerque <lucasalbuquerque01@gmail.com>
2018-09-16 22:00:32 -0300

    Exemplos inicias de orientacao a objetos

---
+++ Projeto02/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02
+{
+    class Triangulo
+    {
+        public double a, b, c;
+        public double area()
+        {
+            double p = (a + b + c) / 2.0;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
