fea575236a104deffa43d3d2d1a6ec987721be00
Lucas Albuquerque <lucasalbuquerque01@gmail.com>
2018-10-03 19:42:53 -0300

    Exercicios de orientacao a objetos

---
+++ ExercicioClasses/ContaBanco.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula02
+{
+    class ContaBanco
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        public ContaBanco(int numero, string titular, double saldo)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        public ContaBanco(int numero, string titular)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = 0.0;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Conta "
+                 + numero
+                 + ", "
+                 + "Titular: "
+                 + titular
+                 + ", "
+                 + "Saldo: $ "
+                 + saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- ExercicioClasses/Program.cs
+++ ExercicioClasses/Program.cs
@@ -7,25 +7,29 @@ namespace Aula02
     {
         static void Main(string[] args)
         {
-            Aluno x = new Aluno();
+            ContaBanco x;
+            
 
-            x.nome = Console.ReadLine();
-            x.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("NOTA FINAL = " + x.notaFinal().ToString("F2", CultureInfo.InvariantCulture));
-
-            if (x.aprovado())
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = (Console.ReadLine());
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string ch = (Console.ReadLine());
+            if(ch == "s")
             {
-                Console.WriteLine("APROVADO");
+                Console.Write("Digite o valor do depósito inicial: ");
+                double DepositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x = new ContaBanco(numero, titular, DepositoInicial);
             }
             else
             {
-                Console.WriteLine("REPROVADO");
-                Console.WriteLine("FALTARAM " + x.quantoFaltaParaAprovacao().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+                x = new ContaBanco(numero, titular);
             }
-            Console.ReadLine();
 
+            Console.WriteLine(x);
+            Console.ReadLine();
+          
 
         }
     }
